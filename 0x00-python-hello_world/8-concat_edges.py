ec36ee193445b42df2a191657e247a9194f1459c
sekaina1234 <soukainasaoud54@gmail.com>
2023-06-05 16:13:17 -0700

    8. Create a new sentence

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-str1 = "Python is an interpreted, interactive, object-oriented programming"
-str2 = " language that combines remarkable power with very clear syntax"
-str = str1[39:67] + str2[0:5] + str1[0:6]
-print(str)
+str = "object-oriented programming with Python"
+print(str[39:-1] + str[0:6])
