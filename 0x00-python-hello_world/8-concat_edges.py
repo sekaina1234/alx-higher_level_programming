7552a3eb482db805a0446b2c3f30b856fefd4ebe
sekaina1234 <soukainasaoud54@gmail.com>
2023-06-05 16:21:14 -0700

    8. Create a new sentenc

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming \
-language that combines remarkable power with very clear syntax"
+str = "Python is an interpreted, interactive, object-oriented programming\
+        language that combines remarkable power with very clear syntax"
+str = " ".join(str.split()[2:5] + str.split()[-2:])
 print(str)
