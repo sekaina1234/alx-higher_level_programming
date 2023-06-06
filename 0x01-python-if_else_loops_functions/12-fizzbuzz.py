fbe9adcd7a966e4a1a4a1486a2fe48f37baecc66
sekaina1234 <soukainasaoud54@gmail.com>
2023-06-06 16:48:31 -0700

    12. Fizz Buzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -4,9 +4,9 @@ def fizzbuzz():
     for n in range(1, 101):
         if n % 3 == 0 and n % 5 == 0:
             output += "FizzBuzz "
-        elif number % 3 == 0:
+        elif n % 3 == 0:
             output += "Fizz "
-        elif number % 5 == 0:
+        elif n % 5 == 0:
             output += "Buzz "
         else:
             output += str(n) + " "
