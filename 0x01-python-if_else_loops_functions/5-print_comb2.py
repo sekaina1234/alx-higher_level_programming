522e53bef121a2dbf69913756dde355547219495
sekaina1234 <soukainasaoud54@gmail.com>
2023-06-06 16:07:48 -0700

    5. 00...99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-# 4-print_hexa.py
-for i in range(100):
-    print("{:02d}".format(i), end=", " if i < 99 else "\n")
+for n in range(0, 100):
+    if n == 99:
+        print("{}".format(n))
+    else:
+        print("{:02}".format(n), end=", ")
