32ddc5246ce4d5e41d3025a3a6c2b16927174799
sekaina1234 <soukainasaoud54@gmail.com>
2023-06-06 15:05:06 -0700

    3. When I was having that alphabet soup, I never thought that it would pay off

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
-vi 3-print_alphabt.py
-for letter in range(ord('a'), ord('z') + 1):
-    if letter != ord('q') and letter != ord('e'):
-        print(chr(letter), end='')
+#!/usr/bin/python3
+for lett in range(ord('a'), ord('z') + 1):
+    if lett != ord('q') and lett != ord('e'):
+        print(chr(lett), end='')
