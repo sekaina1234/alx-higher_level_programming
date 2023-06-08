8db732fd56e2b937d14c8d3362bde5aeb679900a
sekaina1234 <soukainasaoud54@gmail.com>
2023-06-08 02:26:11 -0700

    0. Import a simple function from a simple file

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-a = 1
-b = 2
-from add_0 import add
-result = add(a, b)
-print("{} + {} = {}".format(a, b, result))
+def main():
+    """Print the sum of 1 and 2."""
+    from add_0 import add
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
+if __name__ == "__main__":
+    main()
