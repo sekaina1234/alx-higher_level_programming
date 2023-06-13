a38999df9870d69457e95fb8211421a7d6939ff9
sekaina1234 <soukainasaoud54@gmail.com>
2023-06-12 17:29:52 -0700

    13. Linked list palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -3,6 +3,27 @@
 #include "lists.h"
 
 /**
+ * reverse_list - reverses a linked list
+ * @head: pointer to pointer to the head of the linked list
+ */
+void reverse_list(listint_t **head)
+{
+listint_t *prev = NULL;
+listint_t *current = *head;
+listint_t *next;
+
+while (current != NULL)
+{
+next = current->next;
+current->next = prev;
+prev = current;
+current = next;
+}
+
+*head = prev;
+}
+
+/**
  * is_palindrome - checks if a singly linked list is a palindrome
  * @head: pointer to pointer to the head of the linked list
  * Return: 1 if the linked list is a palindrome, 0 otherwise
@@ -12,10 +33,9 @@ int is_palindrome(listint_t **head)
 listint_t *slow = *head;
 listint_t *fast = *head;
 listint_t *prev = NULL;
-listint_t *temp;
 
 if (*head == NULL || (*head)->next == NULL)
-return (1);
+return (1); /* Empty list or single node is a palindrome */
 
 /* Find the middle node using slow and fast pointers */
 while (fast != NULL && fast->next != NULL)
@@ -25,25 +45,19 @@ prev = slow;
 slow = slow->next;
 }
 
+prev->next = NULL; /* Split the list into two halves */
+
 /* Reverse the second half of the list */
-prev->next = NULL;
-while (slow != NULL)
-{
-temp = slow->next;
-slow->next = prev;
-prev = slow;
-slow = temp;
-}
+reverse_list(&slow);
 
-
-slow = prev;
-fast = *head;
-while (slow != NULL)
+/* Compare the first and second halves of the list */
+while (*head != NULL && slow != NULL)
 {
-if (slow->n != fast->n)
+if ((*head)->n != slow->n)
 return (0);
+
+*head = (*head)->next;
 slow = slow->next;
-fast = fast->next;
 }
 
 return (1);
