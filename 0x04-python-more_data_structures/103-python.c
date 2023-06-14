1ec9b7bd7852cfb78bb841a5185c8202c46ec97d
sekaina1234 <soukainasaoud54@gmail.com>
2023-06-14 16:07:39 -0700

    16. CPython #1: PyBytesObject

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -2,61 +2,52 @@
 #include <stdio.h>
 /**
  * print_python_list - Prints information about a Python list object.
- * @p: Pointer to the PyObject representing the list.
- *
- * This function prints the size and allocated memory of the list, as well as
- * the type and content of each element in the list.
+ * @p: Pointer to the Python list object.
  */
 void print_python_list(PyObject *p)
 {
 Py_ssize_t size, i;
 PyObject *item;
 
-if (!PyList_Check(p))
-{
-printf("[*] Invalid List Object\n");
-return;
-}
-
 size = PyList_Size(p);
 
 printf("[*] Python list info\n");
-printf("[*] Size of the Python List = %ld\n", size);
-printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
+printf("[*] Size of the Python List = %zd\n", size);
+printf("[*] Allocated = %zd\n", ((PyListObject *)p)->allocated);
 
 for (i = 0; i < size; i++)
 {
 item = PyList_GetItem(p, i);
-printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
+printf("Element %zd: %s\n", i, Py_TYPE(item)->tp_name);
 }
 }
 /**
  * print_python_bytes - Prints information about a Python bytes object.
- * @p: Pointer to the PyObject representing the bytes object.
- *
- * This function prints the size and contents of the bytes object.
+ * @p: Pointer to the Python bytes object.
  */
 void print_python_bytes(PyObject *p)
 {
 Py_ssize_t size, i;
-char *str;
+char *string;
+
+printf("[.] bytes object info\n");
 
 if (!PyBytes_Check(p))
 {
-printf("[.] bytes object info\n");
 printf("  [ERROR] Invalid Bytes Object\n");
 return;
 }
 
 size = PyBytes_Size(p);
-str = PyBytes_AsString(p);
+string = PyBytes_AsString(p);
 
-printf("[.] bytes object info\n");
-printf("  size: %ld\n", size);
-printf("  trying string: %s\n", str);
+printf("  size: %zd\n", size);
+printf("  trying string: %s\n", string);
 
-printf("  first %ld bytes: ", size + 1);
-for (i = 0; i < size && i < 10; i++)
-printf("%02x ", (unsigned char)str[i]);
-printf("\n");
+printf("  first %zd bytes: ", (size < 10) ? size + 1 : 10);
+for (i = 0; i < ((size < 10) ? size : 10); i++)
+{
+printf("%02x%c", (unsigned char)string[i],
+(i == 9 || i == size - 1) ? '\n' : ' ');
+}
 }
