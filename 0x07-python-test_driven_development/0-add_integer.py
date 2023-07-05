5dd73b2a15dd31fa5c612545ea7fd39f3125800a
sekaina123 <soukainasaoud54@gmail.com>
2023-07-05 10:44:50 -0700

    0. Integers addition

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,38 +1,29 @@
 #!/usr/bin/python3
 def add_integer(a, b=98):
     """
-    Adds two integers.
+    Return the integer addition of a and b.
 
-    Args:
-        a (int or float): First number.
-        b (int or float): Second number. Default is 98.
-
-    Returns:
-        int: The addition of a and b.
+    Float arguments are typecasted to ints before addition is performed.
 
     Raises:
-        TypeError: If a or b is not an integer or float.
-
+        TypeError: If either of a or b is a non-integer and non-float.
     """
-    if not isinstance(a, (int, float)):
-        raise TypeError("a must be an integer or float")
-    if not isinstance(b, (int, float)):
-        raise TypeError("b must be an integer or float")
+    if ((not isinstance(a, int) and not isinstance(a, float))):
+        raise TypeError("a must be an integer")
+    if ((not isinstance(b, int) and not isinstance(b, float))):
+        raise TypeError("b must be an integer")
+    return (int(a) + int(b))
 
-    return int(a) + int(b)
-
-
-print(add_integer(1, 2))
-print(add_integer(100, -2))
-print(add_integer(2))
-print(add_integer(100.3, -2))
-
-try:
-    print(add_integer(4, "School"))
-except Exception as e:
-    print(e)
-
-try:
-    print(add_integer(None))
-except Exception as e:
-    print(e)
+if __name__ == "__main__":
+    print(add_integer(1, 2))
+    print(add_integer(100, -2))
+    print(add_integer(2))
+    print(add_integer(100.3, -2))
+    try:
+        print(add_integer(4, "School"))
+    except Exception as e:
+        print(e)
+    try:
+        print(add_integer(None))
+    except Exception as e:
+        print(e)
