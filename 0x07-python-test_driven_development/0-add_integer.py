69402499bc46120f838485657be12e9839712422
sekaina123 <soukainasaoud54@gmail.com>
2023-07-05 10:20:31 -0700

    0. Integers addition

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -4,35 +4,19 @@ def add_integer(a, b=98):
     Adds two integers.
 
     Args:
-        a (int): The first integer.
-        b (int, optional): The second integer. Defaults to 98.
+        a (int or float): First number.
+        b (int or float): Second number. Default is 98.
 
     Returns:
-        int: The sum of the two integers.
+        int: The addition of a and b.
 
     Raises:
-        TypeError: If `a` or `b` is not an integer.
+        TypeError: If a or b is not an integer or float.
 
-    Examples:
-        >>> add_integer(1, 2)
-        3
-        >>> add_integer(100, -2)
-        98
-        >>> add_integer(2)
-        100
-        >>> add_integer(100.3, -2)
-        98
-        >>> add_integer(4, "School")
-        Traceback (most recent call last):
-        ...
-        TypeError: b must be an integer
-        >>> add_integer(None)
-        Traceback (most recent call last):
-        ...
-        TypeError: a must be an integer
     """
     if not isinstance(a, (int, float)):
-        raise TypeError("a must be an integer")
+        raise TypeError("a must be an integer or float")
     if not isinstance(b, (int, float)):
-        raise TypeError("b must be an integer")
+        raise TypeError("b must be an integer or float")
+
     return int(a) + int(b)
