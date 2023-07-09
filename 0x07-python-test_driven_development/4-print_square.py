f33bd87326005cbc01fefc76c68b75b0d70135ba
sekaina123 <soukainasaoud54@gmail.com>
2023-07-09 15:41:22 -0700

    1

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,22 +1,24 @@
 #!/usr/bin/python3
-"""
-Defines the function print_square
-"""
+"""Defines a square-printing function."""
 
 
 def print_square(size):
     """
-    Prints a square with the character '#'
+    Print a square with the '#' character.
+
     Args:
-        size (int): Size of the square
+        size (int): The height/width of the square.
+
     Raises:
-        TypeError: If size is not an integer
-        ValueError: If size is less than 0
+        TypeError: If size is not an integer.
+        ValueError: If size is less than 0.
     """
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
 
+    square = '#' * size  # Create a string with '#' repeated size times
+
     for _ in range(size):
-        print("#" * size)
+        print(square)
--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -13,24 +13,16 @@ def text_indentation(text):
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
+    chars = ['.', '?', ':']
     result = ""
-    add_newline = False
-
     for char in text:
-        if add_newline:
-            if char not in ".?:\n":
-                result += "\n"  # Add new line after punctuation
-            add_newline = False
+        result += char
+        if char in chars:
+            result += "\n\n"
 
-        if char == "\n":
-            result += "\n\n"  # Add two new lines after existing new lines
-            add_newline = False
-        elif char in ".?:":
-            result += char
-            add_newline = True
-        else:
-            result += char
+    lines = result.split('\n')
+    for i in range(len(lines)):
+        lines[i] = lines[i].strip()
 
+    result = "\n".join(lines)
     print(result)
-
-text_indentation("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque interdum consequat magna, et fringilla est suscipit in. Nulla commodo felis ac lacus lacinia convallis. Mauris at tincidunt diam? Sed a quam pretium, lacinia sem quis, posuere velit. Pellentesque pulvinar ornare risus, sed venenatis odio tempor at.")
