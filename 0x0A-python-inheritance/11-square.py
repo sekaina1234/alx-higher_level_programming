647d64822abcc49dbd44d6a745349e9e7045e1ac
sekaina123 <soukainasaoud54@gmail.com>
2023-07-10 16:49:49 -0700

    11. Square #2

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Module for Square class"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Represents a square, a special kind of rectangle"""
+
+    def __init__(self, size):
+        """Initializes a Square object
+        Args:
+            size (int): The size of the square's sides"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """Computes the area of the square
+        Returns:
+            The area of the square"""
+        return self.__size ** 2
+
+    def __str__(self):
+        """Returns a string representation of the square"""
+        return "[Square] {}/{}".format(self.__size, self.__size)
