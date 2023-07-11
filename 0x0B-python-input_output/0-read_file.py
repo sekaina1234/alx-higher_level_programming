c360a9875688495af95abd7438ebcc9ddf56bf63
sekaina123 <soukainasaoud54@gmail.com>
2023-07-11 06:22:20 -0700

    0. Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-"""
-Module: 0-read_file
-"""
+"""Module: 0-read_file"""
+
 
 def read_file(filename=""):
-    """
-    Prints the contents of a UTF8 text file to stdout.
+    """Prints the contents of a UTF8 text file to stdout.
 
     Args:
         filename (str): The name of the file to be read. Default is an empty string.
