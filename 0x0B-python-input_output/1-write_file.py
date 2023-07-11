8e200e47b133cf4b50183ccc4c28b14a2cccb121
sekaina123 <soukainasaoud54@gmail.com>
2023-07-11 12:26:59 -0700

    1. Write to a file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Module for writing a string to a text file.
+"""
+
+def write_file(filename="", text=""):
+    """
+    Writes a string to a text file and returns the number of characters written.
+
+    Args:
+        filename (str): The name of the text file. Defaults to an empty string.
+        text (str): The string to be written to the file.
+
+    Returns:
+        int: The number of characters written to the file.
+    """
+    with open(filename, 'w', encoding='utf-8') as file:
+        return file.write(text)
