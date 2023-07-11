593dbb96d67a426bc2bcd998d338080eed49b370
sekaina123 <soukainasaoud54@gmail.com>
2023-07-11 14:30:13 -0700

    7. Load, add, save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,18 +2,12 @@
 """Add all arguments to a Python list
 and save them to a file."""
 import sys
-import json
-
-
-def save_to_json_file(data, filename):
-    with open(filename, 'w') as file:
-        json.dump(data, file)
-
-def load_from_json_file(filename):
-    with open(filename, 'r') as file:
-        return json.load(file)
 
 if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
+
     try:
         items = load_from_json_file("add_item.json")
     except FileNotFoundError:
