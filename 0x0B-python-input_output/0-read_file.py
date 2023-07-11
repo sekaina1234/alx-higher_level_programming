49d5b5b7f4947d21c98e3a7990f43be2e340e8e3
sekaina123 <soukainasaoud54@gmail.com>
2023-07-11 12:17:58 -0700

    0. Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
-"""
-Module for reading and printing the contents of a text file.
-"""
+"""Module for reading and printing the contents of a text file."""
+
 
 def read_file(filename=""):
-    """
-    Function that reads a text file and prints its contents to stdout.
+    """Function that reads a text file and prints its contents to stdout.
 
     Args:
-        filename (str): The name of the text file to be read. Defaults to an empty string.
-    """
+        filename (str): The name of the text file to be read. Defaults to an empty string."""
     with open(filename, 'r', encoding='utf-8') as file:
         print(file.read(), end='')
