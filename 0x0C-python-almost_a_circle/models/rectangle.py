62bac1653e6064c27978412a4863293c21fd3a11
sekaina1234 <soukainasaoud54@gmail.com>
2023-07-17 18:37:26 -0700

    rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -83,8 +83,9 @@ class Rectangle(Base):
 
     def __str__(self):
         """ String representation of the rectangle """
-        return "[Rectangle] ({}) {}/{} - {}/{}".format(
-            self.id, self.x, self.y, self.width, self.height
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
+                                                       self.x, self.y,
+                                                       self.width, self.height)
 
     def update(self, *args, **kwargs):
         """ Assigns values to the attributes """
