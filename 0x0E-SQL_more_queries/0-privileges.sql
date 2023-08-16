bd00b8e9d161a4b1ee82ddbd99a7da65af7e6a57
sekaina1234 <soukainasaoud54@gmail.com>
2023-08-16 14:28:47 -0700

    0. My privileges!

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,12 +1,3 @@
--- Create users
-CREATE USER 'user_0d_1'@'localhost';
-CREATE USER 'user_0d_2'@'localhost';
-
--- Grant privileges to user_0d_1
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-
--- Grant privileges to user_0d_2
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
-
--- Flush privileges to apply changes
-FLUSH PRIVILEGES;
+-- Lists all privileges of the users user_0d_1 and user_0d_2.
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
