1a4efdd37e73b82edaaeac277ef3510496d43205
sekaina1234 <soukainasaoud54@gmail.com>
2023-09-13 12:55:06 -0700

    9. Add

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,14 +1,6 @@
 #!/usr/bin/node
-function add(a, b) {
-  return a + b;
+function add (a, b) {
+  return parseInt(a) + parseInt(b);
 }
 
-const num1 = parseInt(process.argv[2]);
-const num2 = parseInt(process.argv[3]);
-
-if (!isNaN(num1) && !isNaN(num2)) {
-  const result = add(num1, num2);
-  console.log(result);
-} else {
-  console.log('NaN');
-}
+console.log(add(process.argv[2], process.argv[3]));
