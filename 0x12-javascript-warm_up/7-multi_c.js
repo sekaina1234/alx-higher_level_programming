4c94989ef000b885fb1d7fd1a824543bb123d9e0
sekaina1234 <soukainasaoud54@gmail.com>
2023-09-13 11:35:52 -0700

    7. I love C

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-const x = parseInt(process.argv[2]);
+const lang = 'C is fun';
 
-if (!isNaN(x) && x > 0) {
-  for (let i = 0; i < x; i++) {
-    console.log('C is fun');
-  }
-} else {
+if (isNaN(process.argv[2])) {
   console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < parseInt(process.argv[2]); i++) {
+    console.log(lang);
+  }
 }
