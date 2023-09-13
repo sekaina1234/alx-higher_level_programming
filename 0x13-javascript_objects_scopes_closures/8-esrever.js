12e4782bc8912931838d0596203d86d7539aed2b
sekaina1234 <soukainasaoud54@gmail.com>
2023-09-13 16:50:34 -0700

    8. Esrever

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+exports.esrever = function (list) {
+  const reversedList = [];
+  const last = list.length - 1;
+  for (let i = last; i >= 0; i--) {
+    reversedList.push(list[i]);
+  }
+  return (reversedList);
+};
