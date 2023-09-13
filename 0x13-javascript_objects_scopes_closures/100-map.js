bb18fe9f82e809a47b54a84b67d286bf07054345
sekaina1234 <soukainasaoud54@gmail.com>
2023-09-13 16:58:28 -0700

    11. Factor index

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,4 +1,8 @@
 #!/usr/bin/node
-const list = require('./100-data.js').list;
-console.log(list);
-console.log(list.map((item, index) => item * index));
+
+const data = require('./100-data').list;
+
+const mappedData = data.map((value, index) => value * index);
+
+console.log(data);
+console.log(mappedData);
