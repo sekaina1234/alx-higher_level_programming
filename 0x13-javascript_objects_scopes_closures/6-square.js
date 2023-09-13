5df040435aefacae77bac69bb3b5b540a4df2f4a
sekaina1234 <soukainasaoud54@gmail.com>
2023-09-13 16:47:01 -0700

    6. Square #1

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
-const Square5 = require('./5-square');
+const SquareBase = require('./5-square');
 
-class Square extends Square5 {
-  charPrint(c) {
+class Square extends SquareBase {
+  charPrint (c) {
     if (c === undefined) {
       c = 'X';
     }
-
     for (let i = 0; i < this.height; i++) {
       console.log(c.repeat(this.width));
     }
