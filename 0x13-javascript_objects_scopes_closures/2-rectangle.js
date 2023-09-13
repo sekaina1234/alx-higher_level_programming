d10b0f1f0f91fe65f06948ab41b08423667c4331
sekaina1234 <soukainasaoud54@gmail.com>
2023-09-13 15:20:08 -0700

    2. Rectangle #2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
-// class Rectangle that defines a rectangle
+
 class Rectangle {
   constructor(w, h) {
-    if (w > 0 && h > 0) {
+    if (w <= 0 || h <= 0) {
+      return {};
+    } else {
       this.width = w;
       this.height = h;
-    } else {
-      // If either w or h is not a positive integer or equal to 0, create an empty object
-      return {};
     }
   }
 }
