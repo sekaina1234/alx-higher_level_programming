f2057485e2d9f7df787b1f6f4687c1c289f44da9
sekaina1234 <soukainasaoud54@gmail.com>
2023-09-13 13:32:28 -0700

    10. Factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,16 +1,10 @@
 #!/usr/bin/node
-function factorial(n) {
-  if (isNaN(n)) {
+function factorial (n) {
+  if ((isNaN(n)) || (n === 1)) {
     return 1;
+  } else {
+    return n * factorial(n - 1);
   }
-  
-  if (n === 0) {
-    return 1;
-  }
-  
-  return n * factorial(n - 1);
 }
 
-const num = parseInt(process.argv[2]);
-const result = factorial(num);
-console.log(result);
+console.log(factorial(parseInt(process.argv[2])));
