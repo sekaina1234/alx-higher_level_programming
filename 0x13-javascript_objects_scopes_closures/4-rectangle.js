16d4c522979d3ac07449ce1f3dddbbfad5bae87b
sekaina1234 <soukainasaoud54@gmail.com>
2023-09-13 16:35:17 -0700

    4. Rectangle #4

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,29 +1,26 @@
 #!/usr/bin/node
-// class Rectangle that defines
-// a rectangle
+// class Rectangle that defines a rectangle
 class Rectangle {
-  constructor(w, h) {
-    if (w <= 0 || h <= 0) {
-      return {};
-    } else {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
   }
 
-  print() {
+  print () {
     for (let i = 0; i < this.height; i++) {
       console.log('X'.repeat(this.width));
     }
   }
 
-  rotate() {
-    const temp = this.width;
+  rotate () {
+    const aux = this.width;
     this.width = this.height;
-    this.height = temp;
+    this.height = aux;
   }
 
-  double() {
+  double () {
     this.width *= 2;
     this.height *= 2;
   }
