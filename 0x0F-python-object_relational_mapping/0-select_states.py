7949d1f4d09e8f0be5ec1bb060bde8c5bfa2dd4a
sekaina1234 <soukainasaoud54@gmail.com>
2023-09-16 11:23:20 -0700

    0. Get all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
+"""
+Lists all states from the database hbtn_0e_0_usa
+"""
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
-        print("Usage: {} <mysql_username> <mysql_password> <database_name>".format(sys.argv[0]))
+        print("Usage: {} <mysql_username> <mysql_password>
+                <database_name>".format(sys.argv[0]))
         sys.exit(1)
 
     username = sys.argv[1]
