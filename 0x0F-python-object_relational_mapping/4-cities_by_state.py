9e5a31fb3083a50fbc34ccccd1cbfaca9d62145b
sekaina1234 <soukainasaoud54@gmail.com>
2023-09-17 12:27:40 -0700

    4. Cities by states

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,25 +1,39 @@
 #!/usr/bin/python3
 '''
-This script retrieves and lists all cities from a MySQL database.
+script that lists all cities from the database
 '''
 
 import MySQLdb
 import sys
 
-if __name__ == '__main__':
-    db = MySQLdb.connect(
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3],
-        port=3306,
-        host='localhost')
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
 
-    c = db.c()
-    c.execute(
-        'SELECT cities.id, cities.name, states.name FROM cities JOIN states ON\
-        cities.state_id = states.id;')
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
 
-    s = c.fetchall()
+    try:
+        db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
 
-    for state in s:
-        print(state)
+        cursor = db.cursor()
+
+        cursor.execute("""
+            SELECT cities.id, cities.name, states.name
+            FROM cities
+            INNER JOIN states ON cities.state_id = states.id
+            ORDER BY cities.id ASC
+        """)
+
+        results = cursor.fetchall()
+        for row in results:
+            print(row)
+
+        cursor.close()
+        db.close()
+
+    except MySQLdb.Error as e:
+        print("MySQL Error {}: {}".format(e.args[0], e.args[1]))
+        sys.exit(1)
