c741de3fcec939660ff94ad2500a54481d783471
sekaina1234 <soukainasaoud54@gmail.com>
2023-09-17 12:55:04 -0700

    5. All cities by state

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -16,26 +16,22 @@ if __name__ == "__main__":
     database = sys.argv[3]
     state_name = sys.argv[4]
 
-    try:
-        db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+ cursor = db.cursor()
+    cursor.execute(
+        'SELECT cities.name FROM cities\
+        INNER JOIN states ON cities.state_id = states.id\
+        WHERE states.name = %s \
+        ORDER BY cities.id ASC', (sys.argv[4], ))
 
-        cursor = db.cursor()
+    cities = cursor.fetchall()
 
-        cursor.execute("""
-            SELECT GROUP_CONCAT(cities.name SEPARATOR ', ')
-            FROM cities
-            INNER JOIN states ON cities.state_id = states.id
-            WHERE states.name = %s
-            ORDER BY cities.id ASC
-        """, (state_name,))
+    idx = 0
+    for city in cities:
+        if idx != 0:
+            print(", ", end="")
+        print("%s" % city, end="")
+        idx += 1
+    print("")
 
-        result = cursor.fetchone()
-        if result:
-            print(result[0])
-
-        cursor.close()
-        db.close()
-
-    except MySQLdb.Error as e:
-        print("MySQL Error {}: {}".format(e.args[0], e.args[1]))
-        sys.exit(1)
+    cursor.close()
+    db.close()
