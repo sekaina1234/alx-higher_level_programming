12f911441c83c8b0e5b574d6fa49e3f05852eda6
sekaina1234 <soukainasaoud54@gmail.com>
2023-09-17 10:41:45 -0700

    1. Filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-#!/usr/bin/python3
 '''
 lists all states with a name starting with N
 '''
@@ -9,15 +8,12 @@ import sys
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
-        print("Usage: {} <username> <password>
-                <database>".format(sys.argv[0]))
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
         sys.exit(1)
 
-    username, password, database_name = sys.argv[1],
-    sys.argv[2], sys.argv[3]
+    username, password, database_name = sys.argv[1], sys.argv[2], sys.argv[3]
 
-    db = MySQLdb.connect(host="localhost", port=3306,
-            user=username, passwd=password, db=database_name)
+    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database_name)
 
     cursor = db.cursor()
 
