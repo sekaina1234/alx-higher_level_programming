232f7d2a7425446bf3388b112584bd06a0f69878
sekaina1234 <soukainasaoud54@gmail.com>
2023-09-17 11:17:38 -0700

    2. Filter states by user input

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -22,8 +22,7 @@ if __name__ == "__main__":
 
     cursor = db.cursor()
 
-    query = "SELECT * FROM states WHERE name LIKE
-             BINARY %s ORDER BY id ASC"
+    query = "SELECT * FROM states WHERE name LIKE BINARY %s ORDER BY id ASC"
 
     cursor.execute(query, (state_name,))
 
