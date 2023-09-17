edd4dd39d62f6db158a20932065620e6c9888357
sekaina1234 <soukainasaoud54@gmail.com>
2023-09-17 12:56:42 -0700

    5. All cities by state

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -6,17 +6,15 @@ script that takes in the name of a state as an argument and lists all cities
 import MySQLdb
 import sys
 
-if __name__ == "__main__":
-    if len(sys.argv) != 5:
-        print("Usage: {} <username> <password> <database> <state_name>".format(sys.argv[0]))
-        sys.exit(1)
+if __name__ == '__main__':
+    db = MySQLdb.connect(
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        port=3306,
+        host='localhost')
 
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    state_name = sys.argv[4]
-
- cursor = db.cursor()
+    cursor = db.cursor()
     cursor.execute(
         'SELECT cities.name FROM cities\
         INNER JOIN states ON cities.state_id = states.id\
