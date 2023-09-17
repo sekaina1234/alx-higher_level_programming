25ae5edb97d992c7d28be5e895040bd205e78b67
sekaina1234 <soukainasaoud54@gmail.com>
2023-09-17 12:41:56 -0700

    4. Cities by states

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -16,7 +16,8 @@ if __name__ == "__main__":
     database = sys.argv[3]
 
     try:
-        db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+        db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password,
+                db=database)
 
         cursor = db.cursor()
 
