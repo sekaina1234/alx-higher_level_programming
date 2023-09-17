ea05618164e2c4e779836fd86f6946881c107d92
sekaina1234 <soukainasaoud54@gmail.com>
2023-09-17 13:35:27 -0700

    9. Contains

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -3,32 +3,20 @@
 database hbtn_0e_6_usa.
 """
 
-import sys
-from model_state import Base, State
+from sys import argv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from model_state import State
 
-if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database>"
-              .format(sys.argv[0]))
-        sys.exit(1)
 
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
+if __name__ == '__main__':
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3]))
+    InstanceSession = sessionmaker(bind=engine)
+    session = InstanceSession()
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(username, password, database),
-                           pool_pre_ping=True)
-
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    states_with_a = session.query(State).filter(State.name
-                    .like('%a%')).order_by(State.id).all()
-    
-    for state in states_with_a:
-        print("{}: {}".format(state.id, state.name))
-
+    states = session.query(State).filter(
+        State.name.contains('a')).order_by(State.id).all()
+    for state in states:
+        print('{}: {}'.format(state.id, state.name))
     session.close()
