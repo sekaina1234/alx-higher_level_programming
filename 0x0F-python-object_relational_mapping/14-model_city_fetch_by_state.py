0c2234f23979c9ea16edfec589fb376e23640bf1
sekaina1234 <soukainasaoud54@gmail.com>
2023-09-17 14:05:27 -0700

    14. Cities in state

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-"""Print all City objects from the database hbtn_0e_14_usa.
-"""
+'''
+Prints all City objects
+'''
+
+
 
 from sys import argv
 from sqlalchemy import create_engine
