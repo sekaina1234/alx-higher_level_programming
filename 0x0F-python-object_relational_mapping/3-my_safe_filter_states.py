d57973720e223a5bd7a48e4085b6084aea74164e
sekaina1234 <soukainasaoud54@gmail.com>
2023-09-17 11:34:14 -0700

    3. SQL Injection...

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -7,25 +7,23 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    if len(sys.argv) != 5:
-        print("Usage: {} <username> <password> <database> <state_name>".format(sys.argv[0]))
-        sys.exit(1)
+if len(sys.argv) != 5:
+print("Usage: {} <username> <password> <database> <state_name>".format(sys.argv[0]))
+sys.exit(1)
 
-    username, password, database_name, state_name = sys.argv[1],
-                                                    sys.argv[2], sys.argv[3], sys.argv[4]
+username, password, database_name, state_name = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
 
-    db = MySQLdb.connect(host="localhost", port=3306,
-         user=username, passwd=password, db=database_name)
+db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database_name)
 
-    cursor = db.cursor()
+cursor = db.cursor()
 
-    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
 
-    cursor.execute(query, (state_name,))
+cursor.execute(query, (state_name,))
 
-    results = cursor.fetchall()
-    for row in results:
-        print(row)
+results = cursor.fetchall()
+for row in results:
+print(row)
 
-    cursor.close()
-    db.close()
+cursor.close()
+db.close()
