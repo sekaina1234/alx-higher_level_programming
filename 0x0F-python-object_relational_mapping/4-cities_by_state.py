db2c6d792c9c60b5bdcb1800614cc3dc17e6cfde
sekaina1234 <soukainasaoud54@gmail.com>
2023-09-17 12:59:06 -0700

    4. Cities by states

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -6,32 +6,20 @@ script that lists all cities from the database
 import MySQLdb
 import sys
 
-if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
-        sys.exit(1)
+if __name__ == '__main__':
+    db = MySQLdb.connect(
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        port=3306,
+        host='localhost')
 
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
-    try:
-        db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password,
-                db=database)
-
-        cursor = db.cursor()
-
-        cursor.execute(
+    cursor = db.cursor()
+    cursor.execute(
         'SELECT cities.id, cities.name, states.name FROM cities JOIN states ON\
         cities.state_id = states.id;')
 
-        results = cursor.fetchall()
-        for row in results:
-            print(row)
+    states = cursor.fetchall()
 
-        cursor.close()
-        db.close()
-
-    except MySQLdb.Error as e:
-        print('MySQL Error {}: {}'.format(e.args[0], e.args[1]))
-        sys.exit(1)
+    for state in states:
+        print(state)
