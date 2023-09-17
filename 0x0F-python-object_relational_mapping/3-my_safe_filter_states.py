29ed39efeaf3ec612a8241a8a24441456cd3bcb4
sekaina1234 <soukainasaoud54@gmail.com>
2023-09-17 11:58:19 -0700

    3. SQL Injection...

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -11,10 +11,10 @@ if __name__ == "__main__":
         print("Usage: {} <username> <password> <database> <state_name>".format(sys.argv[0]))
         sys.exit(1)
 
-    username, password, database_name, state_name = sys.argv[1],
-                                                    sys.argv[2], sys.argv[3], sys.argv[4]
+    username, password, database_name, state_name = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
 
-    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database_name)
+    db = MySQLdb.connect(host="localhost", port=3306, user=username,
+            passwd=password, db=database_name)
 
     cursor = db.cursor()
 
