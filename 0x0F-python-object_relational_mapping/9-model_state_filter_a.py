04d55b70e03d6479618ce07d1fe62d95e42f461b
sekaina1234 <soukainasaoud54@gmail.com>
2023-09-17 13:33:32 -0700

    9. Contains

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -10,7 +10,8 @@ from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        print("Usage: {} <username> <password> <database>"
+              .format(sys.argv[0]))
         sys.exit(1)
 
     username = sys.argv[1]
