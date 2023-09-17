c35ccde186b418dca5c51d5b8797262c8df2542e
sekaina1234 <soukainasaoud54@gmail.com>
2023-09-17 10:20:18 -0700

    0. Get all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,35 +1,30 @@
 #!/usr/bin/python3
-"""
-Lists all states from the database hbtn_0e_0_usa
-"""
 import MySQLdb
 import sys
 
-if __name__ == "__main__":
+if __name__ == "___main___":
     if len(sys.argv) != 4:
-        print("Usage: {} <mysql_username> <mysql_password>
-                <database_name>".format(sys.argv[0]))
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
         sys.exit(1)
 
     username = sys.argv[1]
     password = sys.argv[2]
-    database_name = sys.argv[3]
+    database = sys.argv[3]
 
-    try:
-        db = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=username,
-            passwd=password,
-            db=database_name
-        )
-        cursor = db.cursor()
-        cursor.execute("SELECT * FROM states ORDER BY id;")
-        states = cursor.fetchall()
-        for state in states:
-            print(state)
-        cursor.close()
-        db.close()
-    except MySQLdb.Error as e:
-        print("MySQL Error {}: {}".format(e.args[0], e.args[1]))
-        sys.exit(1)
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+    for row in cursor.fetchall():
+        print(row)
+
+    cursor.close()
+    db.close()
