1ea5efb6318ff6abf084b3fd2b30d17726c20858
sekaina1234 <soukainasaoud54@gmail.com>
2023-09-29 10:09:43 -0700

    0. What's my status? #0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import urllib.request
+
+url = 'https://alx-intranet.hbtn.io/status'
+
+try:
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type:", type(body))
+        print("\t- content:", body)
+        print("\t- utf8 content:", body.decode('utf-8'))
+except Exception as e:
+    print("Error:", e)
