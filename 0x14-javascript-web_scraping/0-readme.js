4365595329c23016c7a7ce62fcb1952c274371b4
sekaina1234 <soukainasaoud54@gmail.com>
2023-10-24 03:06:45 -0700

    0. Readme

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+
+const filePath = process.argv[2];
+
+if (!filePath) {
+  console.error('Usage: node 0-readme.js <file-path>');
+  process.exit(1);
+}
+
+fs.readFile(filePath, 'utf-8', (err, data) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(data);
+  }
+});
