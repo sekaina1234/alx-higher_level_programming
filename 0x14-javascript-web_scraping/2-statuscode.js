b48938dae6be38bff27de5e3f308416f2131f17b
sekaina1234 <soukainasaoud54@gmail.com>
2023-10-24 14:16:26 -0700

    2. Status code

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const request = require('request');
+
+const url = process.argv[2];
+
+if (!url) {
+  console.log('Usage: node 2-statuscode.js <URL>');
+} else {
+  request(url, (error, response) => {
+    if (error) {
+      console.error(error);
+    } else {
+      console.log(`code: ${response.statusCode}`);
+    }
+  });
+}
