8285b859850f8d8edf9df734d2464575779d8d3d
sekaina1234 <soukainasaoud54@gmail.com>
2023-10-24 14:10:03 -0700

    1. Write me

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+const filePath = process.argv[2];
+const content = process.argv[3];
+
+if (!filePath || !content) {
+  console.log('Usage: node 1-writeme.js <file-path> <content>');
+} else {
+  fs.writeFile(filePath, content, 'utf-8', (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log('File written successfully');
+    }
+  });
+}
