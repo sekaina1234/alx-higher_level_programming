6fa9260a836ae468040785ee8554615faecf8143
sekaina1234 <soukainasaoud54@gmail.com>
2023-10-24 15:48:53 -0700

    8. Right order

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,42 +1,45 @@
 #!/usr/bin/node
 const request = require('request');
 
-const movieId = process.argv[2];
-
-if (!movieId) {
-  console.log('Usage: node 101-starwars_characters.js <Movie ID>');
-} else {
-  const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
-
-    request(apiUrl, (error, response, body) => {
-    if (error) {
-      console.error(error);
-    } else if (response.statusCode !== 200) {
-      console.error(`Request failed with status code: ${response.statusCode}`);
-    } else {
-      const movieData = JSON.parse(body);
-      const characterUrls = movieData.characters;
-
-      function fetchCharacter(index) {
-        if (index < characterUrls.length) {
-          request(characterUrls[index], (characterError, characterResponse, characterBody) => {
-            if (characterError) {
-              console.error(characterError);
-            } else if (characterResponse.statusCode !== 200) {
-              console.error(`Character request failed with status code: ${characterResponse.statusCode}`);
-            } else {
-              const characterData = JSON.parse(characterBody);
-              console.log(characterData.name);
-            }
-
-            // Fetch the next character
-            fetchCharacter(index + 1);
-          });
-        }
+function getDataFrom (url) {
+  return new Promise(function (resolve, reject) {
+    request(url, function (err, _res, body) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(body);
       }
-
-      // Start fetching characters from the first character URL
-      fetchCharacter(0);
-    }
+    });
   });
 }
+
+function errHandler (err) {
+  console.log(err);
+}
+
+function printMovieCharacters (movieId) {
+  const movieUri = `https://swapi-api.hbtn.io/api/films/${movieId}`;
+
+  getDataFrom(movieUri)
+    .then(JSON.parse, errHandler)
+    .then(function (res) {
+      const characters = res.characters;
+      const promises = [];
+
+      for (let i = 0; i < characters.length; ++i) {
+        promises.push(getDataFrom(characters[i]));
+      }
+
+      Promise.all(promises)
+        .then((results) => {
+          for (let i = 0; i < results.length; ++i) {
+            console.log(JSON.parse(results[i]).name);
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    });
+}
+
+printMovieCharacters(process.argv[2]);
