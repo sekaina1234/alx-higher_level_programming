25828e579a0b505c350115cd280db91a0a393c0f
sekaina1234 <soukainasaoud54@gmail.com>
2023-11-02 08:28:39 -0700

    11. List, add, remove

---
+++ 0x15-javascript-web_jquery/101-script.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  // Add item to the list
+  $('#add_item').click(function() {
+    $('ul.my_list').append('<li>Item</li>');
+  });
+
+  // Remove the last item from the list
+  $('#remove_item').click(function() {
+    $('ul.my_list li:last-child').remove();
+  });
+
+  // Clear the entire list
+  $('#clear_list').click(function() {
+    $('ul.my_list').empty();
+  });
+});
